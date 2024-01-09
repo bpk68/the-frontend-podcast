1f406f0e40c757df9389c9277da9b716b1b06590
Rob Kendal <38687284+bpk68@users.noreply.github.com>
2024-01-09 15:29:50 +0000

    Updataes compression in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
 import { defineConfig } from 'astro/config';
+import prefetch from '@astrojs/prefetch';
+import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  compressHTML: true,
+  site: 'https://thefrontendpodcast.site',
+  integrations: [sitemap(), prefetch(), compress({ logger: 1 })],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+      wrap: true,
+    },
+  },
+  vite: {
+    build: {
+      sourcemap: true,
+    },
+  },
+});
